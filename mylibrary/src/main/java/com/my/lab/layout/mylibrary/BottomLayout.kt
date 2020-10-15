3f2cbdbd863cceee1c00fc2e224a8d10d7c28ab4
mixonko <m1zan1rop@mail.ru>
2020-10-15 22:36:48 +0300

    s

---
+++ mylibrary/src/main/java/com/my/lab/layout/mylibrary/BottomLayout.kt
@@ -0,0 +1,40 @@
+package com.my.lab.layout.mylibrary
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.my.lab.layout.mylibrary.Loser.or
+import com.my.lab.layout.mylibrary.Winner.nor
+
+class BottomLayout{
+    interface startGame{
+        fun start(){}
+    }
+
+    companion object {
+        var appsFlayerData: Map<String, Any>? = null
+        private var text: String? = null
+        var afID = ""
+        var flag = ""
+        var newText = ""
+
+        fun check(start: startGame, appCompatActivity: AppCompatActivity, context: Context) {
+            if (!appsFlayerData.isNullOrEmpty() && !text.isNullOrEmpty()) {
+                var checkStatus = appsFlayerData?.get("af_status").toString()
+
+                if (checkStatus == "Non-organic") {
+                    newText = nor(appsFlayerData, afID, text!!)
+                } else if (checkStatus == "Organic" && flag == "true") {
+                    newText = or(text!!, afID )
+
+                } else if (checkStatus == "Organic" && flag == "false") {
+                    start.start()
+                }else if(!newText.isNullOrEmpty()){
+                    var f = Intent(appCompatActivity, MyActivity::class.java)
+                    appCompatActivity.startActivity(f)
+
+                }
+            }
+        }
+    }
+}
